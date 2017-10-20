cc2f1d122dbdd57dff1efcaecf8ab51af23e4004
bniebuhr <bernardo_brandaum@yahoo.com.br>
2017-10-20 00:28:06 -0200

    widened Windows GUI 6

--- _LSMetrics_v1_0_0_stable/LSMetrics_v1_0_0.py
+++ _LSMetrics_v1_0_0_stable/LSMetrics_v1_0_0.py
@@ -2951,7 +2951,7 @@ if __name__ == "__main__":
     
     # Adjusting width of GUI depending on the Operational System
     if CURRENT_OS == "Windows":
-      size = (530, 820)
+      size = (530, 810)
     elif CURRENT_OS == "Linux":
       size = (530 + 50, 770)
     elif CURRENT_OS == "Darwin": # For Mac
