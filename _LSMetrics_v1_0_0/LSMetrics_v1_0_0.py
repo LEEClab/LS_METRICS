cf42408e1691bbfbb2f5f39d1e49d907a3848abb
bniebuhr <bernardo_brandaum@yahoo.com.br>
2017-10-24 12:20:19 -0200

    add height GUI in windows 3

--- _LSMetrics_v1_0_0/LSMetrics_v1_0_0.py
+++ _LSMetrics_v1_0_0/LSMetrics_v1_0_0.py
@@ -2983,7 +2983,7 @@ if __name__ == "__main__":
     
     # Adjusting width of GUI depending on the Operational System
     if CURRENT_OS == "Windows":
-      size = (530, 930)
+      size = (530, 850)
     elif CURRENT_OS == "Linux":
       size = (530 + 50, 770)
     elif CURRENT_OS == "Darwin": # For Mac
